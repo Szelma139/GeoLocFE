151e264bdc7abc35a9f098d0da3cfdcd6f1be841
Szelma139 <raduniek1@gmail.com>
2024-03-22 11:37:06 +0100

    add image

--- src/components/ShortInfo.tsx
+++ src/components/ShortInfo.tsx
@@ -2,13 +2,15 @@ import { hasFlag } from "country-flag-icons";
 import { ResponseType } from "../ResponseType";
 
 export const ShortInfo = (props: ResponseType) => {
-  const { countryCode, lat, lon, region } = props;
+  const { countryCode, lat, lon, city, region } = props;
   console.log(props);
   return (
     <div>
       <p> Latitude: {lat}</p>
       <p> Longitude: {lon}</p>
       <p> Region {region}</p>
+      <p> Country code {countryCode}</p>
+      <p> City {city}</p>
       <div>
         {hasFlag(countryCode) === true && (
           <img
